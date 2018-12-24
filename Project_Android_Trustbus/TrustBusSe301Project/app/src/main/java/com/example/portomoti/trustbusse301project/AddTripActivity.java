618f967afe3c432f842e34de43575f5350add4c6
Can Utku <33282009+cutku@users.noreply.github.com>
2018-12-24 23:24:18 +0300

    Calender view||selection Added/ Add Trip Fixed/Date changed to String of some problems

--- Project_Android_Trustbus/TrustBusSe301Project/app/src/main/java/com/example/portomoti/trustbusse301project/AddTripActivity.java
+++ Project_Android_Trustbus/TrustBusSe301Project/app/src/main/java/com/example/portomoti/trustbusse301project/AddTripActivity.java
@@ -7,8 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.ParseException;
@@ -23,18 +25,35 @@ public class AddTripActivity extends AppCompatActivity {
     EditText dateText;
     String Cities[] = {"Istanbul", "Ankara", "Izmir", "Diyarbakır"};
     ArrayAdapter<String> adapter;
-
+    Button calenderButton;
     String recordFrom = "";
     String recordWhere = "";
+
+    String DateALL;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_trip);
 
+
+
+
+
+        //calender activity get date from calender
+        TextView dateTimeText = findViewById(R.id.addTripActivityTextView);
+
+        String calenderDate = getIntent().getStringExtra("calenderDate");
+        if (calenderDate!= null) {
+            dateTimeText.setText(calenderDate + " 12:00");
+                    DateALL = calenderDate + " 12:00"; //now only add 12:00 clock
+        }
+
+        //calender Activity End
         fromSpinner = findViewById(R.id.addTripActivityFromSpinner);
         whereSpinner = findViewById(R.id.addTripActivityWhereSpinner);
-        dateText = findViewById(R.id.addTripActivityDateTimeText);
-
+        calenderButton = findViewById(R.id.addTripActivityDateButton);
         adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,Cities);
 
         fromSpinner.setAdapter(adapter);
@@ -62,6 +81,7 @@ public class AddTripActivity extends AppCompatActivity {
 
             }
         });
+
         whereSpinner.setAdapter(adapter);
         whereSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -89,20 +109,24 @@ public class AddTripActivity extends AppCompatActivity {
         });
     }
 
+    public void goCalender(View view){
+        Intent intent = new Intent(getApplicationContext(), CalenderActivity.class);
+        startActivity(intent);
+    }
 
 
     public void addTrip(View view) throws java.text.ParseException {
 
      //   String textFrom = fromText.getText().toString();
      //   String textDestionation = destinationText.getText().toString();
-        String textDate = dateText.getText().toString();
+      //  String textDate = dateText.getText().toString();
 
-        Date date1 = new SimpleDateFormat("dd/MM/yyyy HH:mm").parse(textDate);
+//        Date date1 = new SimpleDateFormat("dd/MM/yyyy HH:mm").parse(textDate);
 
         ParseObject object = new ParseObject("Trips");
         object.put("from", recordFrom);
         object.put("destination", recordWhere);
-        object.put("date", date1);
+        object.put("date", DateALL);
         object.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
@@ -138,7 +162,6 @@ public class AddTripActivity extends AppCompatActivity {
                 }
             }
         });
-
 
     }
 }
